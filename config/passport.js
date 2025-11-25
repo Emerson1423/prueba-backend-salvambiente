7f395dd8f9dc7e56f51ac543e0f7d34d06cb428a
Emerson1423 <emersonaldahir2453@gmail.com>
2025-11-25 08:35:17 -0600

    Update Google OAuth callback URL

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const pool = require('../bd');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/api/auth/google/callback"
+    callbackURL: "https://prueba-backend-salvambiente.onrender.com/api/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
